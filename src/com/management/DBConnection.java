5786f19e72d774d700717ba9c83a01c36f4daa73
Manoj Kumar C <manojkumarc7805@gmail.com>
2026-04-26 11:05:00 +0530

    Initial commit

---
+++ src/com/management/DBConnection.java
@@ -0,0 +1,19 @@
+package com.management;
+
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/management_db",
+                "root",
+                "Manoj@123" // change this
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Anudip {
+	requires java.desktop;
+	requires java.sql;
+}
